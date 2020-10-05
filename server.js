d8082a2d56ad99a9e7a0b87affb87c490bdf3a94
distributhor <distributhor@fastmail.com>
2020-10-05 11:08:36 +0100

    Implement signature check on raw urlencoded body

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const service = require("./service");
 const app = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true, verify: service.saveRawBodyForRequest }));
 
 app.get("/health-check", (req, res) => {
   res.sendStatus(200);
@@ -20,7 +20,7 @@ app.post("/webhook/json", service.verifyGithubJsonRequest, (req, res) => {
     console.log("GitHub JSON request verification success");
     res.sendStatus(200);
   } catch (e) {
-    console.log("GitHub JSON request verification failure");
+    console.log("GitHub JSON request verification error");
     console.log(e);
     return res.sendStatus(503);
   }
@@ -31,7 +31,7 @@ app.post("/webhook/urlencoded", service.verifyGithubUrlEncodedRequest, (req, res
     console.log("GitHub URLENCODED request verification success");
     res.sendStatus(200);
   } catch (e) {
-    console.log("GitHub URLENCODED request verification failure");
+    console.log("GitHub URLENCODED request verification error");
     console.log(e);
     return res.sendStatus(503);
   }
--- service.js
+++ service.js
@@ -3,6 +3,12 @@ const crypto = require("crypto");
 
 require("dotenv").config({ path: path.join(__dirname, "/.env") });
 
+function saveRawBodyForRequest(req, res, buf, encoding) {
+  if (buf && buf.length) {
+    req.rawBody = buf.toString(encoding || "utf8");
+  }
+}
+
 function verifyGithubJsonRequest(req, res, next) {
   console.log("verifyGithubJsonRequest");
   console.log(req.headers);
@@ -30,8 +36,8 @@ function verifyGithubJsonRequest(req, res, next) {
       .digest("hex");
 
   if (req.headers["x-hub-signature"] != signature) {
-    console.log("Signature does not match");
-    return res.sendStatus(503);
+    console.log("Signatures do not match");
+    return res.sendStatus(403);
   }
 
   next();
@@ -40,6 +46,7 @@ function verifyGithubJsonRequest(req, res, next) {
 function verifyGithubUrlEncodedRequest(req, res, next) {
   console.log("verifyGithubUrlEncodedRequest");
   console.log(req.headers);
+  console.log(req.rawBody);
   console.log(req.body);
 
   if (!process.env.WEBHOOK_SECRET) {
@@ -51,25 +58,21 @@ function verifyGithubUrlEncodedRequest(req, res, next) {
     return res.sendStatus(503);
   }
 
-  const payload = JSON.stringify(req.body);
-  if (!payload) {
-    return res.sendStatus(503);
-  }
-
   const signature =
     "sha1=" +
     crypto
       .createHmac("sha1", process.env.WEBHOOK_SECRET)
-      .update(payload)
+      .update(req.rawBody)
       .digest("hex");
 
   if (req.headers["x-hub-signature"] != signature) {
-    console.log("Signature does not match");
-    return res.sendStatus(503);
+    console.log("Signatures do not match");
+    return res.sendStatus(403);
   }
 
   next();
 }
 
+exports.saveRawBodyForRequest = saveRawBodyForRequest;
 exports.verifyGithubJsonRequest = verifyGithubJsonRequest;
 exports.verifyGithubUrlEncodedRequest = verifyGithubUrlEncodedRequest;
